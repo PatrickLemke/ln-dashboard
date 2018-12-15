fd4f4a33cbe42b60bc3c28e37e46d9bd5c12af4f
Patrick Lemke <patricklemke95@gmail.com>
2018-12-15 14:42:40 +0100

    Added chart.js demo

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
     // Display tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+    drawFeeChart([3000, 1100, 80, 6000, 500, 100], ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+
 });
+
+function drawFeeChart(data, labels) {
+    const ctx = document.getElementById("feeChart").getContext('2d');
+
+    const feeChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Fees Earned',
+                data: data,
+                borderColor: '#195D9E',
+                backgroundColor: 'white',
+                fill: false,
+                lineTension: 0,
+            }]
+        },
+        options: {
+            legend: false,
+            responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
